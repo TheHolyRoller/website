3dc962a632eb58757b2d9c943f2f534670f9f496
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2023-10-20 22:08:13 +0200

    Just fixed some build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
 import Image from 'next/image'
 
-import Content from './Components/Content'; 
 import LandingPage from './Components/LandingPage'; 
 
 export default function Home() {
   return (
    
-   <div p='0' m='0' style={{background: '#161822'}} >
+   <div>
    
    <LandingPage/> 
-
-    
+   
    </div>
    
    
